f09d26565a45f116bc59efaa9a62e445c4e33fc5
Gabriel Resende <gabriellrr2005@gmail.com>
2023-07-18 14:15:05 -0300

    Project created

---
+++ src/main/java/com/resendegabriel/serieslist/SerieslistApplication.java
@@ -0,0 +1,13 @@
+package com.resendegabriel.serieslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerieslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerieslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resendegabriel/serieslist/SerieslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resendegabriel.serieslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerieslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
